303dcb7e47243a36641619874e6c8ce0c61884f3
Carlos A. Pinzon <carlos.pinzon@quantomm.tech>
2021-10-15 17:31:06 -0500

    change var by let

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let nit = "890300279"; // Carry out the necessary validations on the captured number. CAPM.
+let nit = "79886653"; // Carry out the necessary validations on the captured number. CAPM.
 const base = [3,7,13,17,19,23,29,37,41,43,47,53,59,67,71];
 const DIVIDER = 11;
 let checkDigit;
@@ -7,14 +7,14 @@ let accumulator = 0;
 let reverseString = (str) => str.split("").reverse();
 
 
-var arrayString = reverseString(nit);
+let arrayString = reverseString(nit);
 
 for (let index = 0; index < arrayString.length; index++) {
     let calculation = Number(arrayString[index]) * base[index]; 
     accumulator += calculation;   
 }
  
-var calculationBasis = accumulator % DIVIDER;
+let calculationBasis = accumulator % DIVIDER;
 checkDigit = calculationBasis > 1 ? DIVIDER - calculationBasis: calculationBasis;
 
 console.log(checkDigit);
