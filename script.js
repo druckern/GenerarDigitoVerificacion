cc1d708078755faadb6543e9426a897a85ff20de
Carlos A. Pinzon <carlos.pinzon@quantomm.tech>
2021-10-15 12:38:17 -0500

    I include script that calculates the check digit

---
+++ script.js
@@ -0,0 +1,20 @@
+let nit = "890300279"; // Carry out the necessary validations on the captured number. CAPM.
+const base = [3,7,13,17,19,23,29,37,41,43,47,53,59,67,71];
+const DIVIDER = 11;
+let checkDigit;
+let accumulator = 0;
+
+let reverseString = (str) => str.split("").reverse();
+
+
+var arrayString = reverseString(nit);
+
+for (let index = 0; index < arrayString.length; index++) {
+    let calculation = Number(arrayString[index]) * base[index]; 
+    accumulator += calculation;   
+}
+ 
+var calculationBasis = accumulator % DIVIDER;
+checkDigit = calculationBasis > 1 ? DIVIDER - calculationBasis: calculationBasis;
+
+console.log(checkDigit);
